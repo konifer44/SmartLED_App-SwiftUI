3757972bbcf6badf8127047ab707ea7fd67f2ccb
konifer44 <konifer44@gmail.com>
2021-03-26 16:40:58 +0100

    Initial Commit

---
+++ Bluetooth_SmartLED/Bluetooth_SmartLEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bluetooth_SmartLEDApp.swift
+//  Bluetooth_SmartLED
+//
+//  Created by Jan Konieczny on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bluetooth_SmartLEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bluetooth_SmartLED/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bluetooth_SmartLED
+//
+//  Created by Jan Konieczny on 26/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
